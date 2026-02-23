7ab2240bb11883de6382f3901c6c89c5f9254193
seblin-1230 <sebastian.lindqvist@elev.ga.ntig.se>
2026-02-23 10:46:00 +0100

    More work on station communication

--- program/initilize_station.lua
+++ program/initilize_station.lua
@@ -1,5 +1,7 @@
 local cc_strings = require("cc.strings")
 
+local drive = assert(peripheral.find("drive"), "Please connect disk drive to begin setup")
+
 local function check_peripherals()
     local needed_peripherals = {
         "modem",
@@ -34,10 +36,10 @@ local function check_peripherals()
 end
 
 local function define_settings()
-    settings.define("station_id", { description = "The station id", type = "number" })
-    settings.define("station_description", { description = "The stations description", type = "string" })
-    settings.define("arrival_position", { description = "The position a user should arrive on when the station is the destination", type = "table" })
-    settings.define("transfer_position", { description = "The position a user should arrive on when the station is used as an inbetween", type = "table" })
+    settings.define("station_data", { description = "The station id", type = "table" })
+    -- settings.define("station_description", { description = "The stations description", type = "string" })
+    -- settings.define("arrival_position", { description = "The position a user should arrive on when the station is the destination", type = "table" })
+    -- settings.define("transfer_position", { description = "The position a user should arrive on when the station is used as an inbetween", type = "table" })
 
     write("Station desc: ")
     settings.set("station_description", read())
@@ -53,20 +55,48 @@ local function define_settings()
     settings.set("transfer_position", vector.new(assert(tonumber(pos[1])), assert(tonumber(pos[2])), assert(tonumber(pos[3]))))
     print()
 
-    rednet.host("add station", settings.get("transfer_position"))
+    local controller_id = assert(rednet.lookup("add station", "station_controller"))
+    rednet.send(controller_id, {description = settings.get("station_description"), arrival_coordinates = settings.get("arrival_position"), transfer_coordinates = settings.get("transfer_position")}, "add station")
 
-    local controller_id = rednet.lookup("add station", "station_controller")
-    print(controller_id, type(controller_id))
+    local station_data
+    repeat
+        local id, message = rednet.receive("stations")
+
+        station_data = message
+    until id == controller_id
+
+    settings.set("station_id", station_id)
+    print("Station id: " .. tostring(station_id))
+
+    return true
 end
 
 
 if check_peripherals() then goto quit end
 print()
 
-
 local modem = assert(peripheral.find("modem"))
 rednet.open(peripheral.getName(modem))
 
+term.setTextColor(colors.green)
+print("Disk found")
+local disk_path = drive.getMountPath()
+
+fs.makeDir("station")
+
+fs.copy(disk_path .. "/program/sha256.lua", "station/sha256.lua")
+print("sha256.lua copied...")
+sleep(0.1)
+
+fs.copy(disk_path .. "/program/validate.lua", "station/validate.lua")
+print("validate.lua copied...")
+sleep(0.1)
+
+fs.copy(disk_path .. "/program/station.lua", "station/station.lua")
+print("station.lua copied...\n")
+sleep(0.1)
+
+term.setTextColor(colors.white)
 local settings_defined = define_settings()
 
 ::quit::
---
+++ program/station_controller.lua
@@ -0,0 +1,50 @@
+local hostname = "station_controller"
+
+local disk_drive = assert(peripheral.find("drive"), "No drive connected")
+
+rednet.open("top")
+
+disk_drive.setDiskLabel("Stations")
+
+local stations = {}
+
+local function read_station_file()
+    local stations_file = assert(fs.open(disk_drive.getMountPath() .. "/stations", "r"))
+    stations = textutils.unserialise(stations_file.read() --[[@as string]]) --[[@as table]]  or {}
+    stations_file.close()
+end
+
+local function write_station_file()
+    local stations_file = assert(fs.open(disk_drive.getMountPath() .. "/stations", "w"))
+    stations_file.write(textutils.serialise(stations))
+    stations_file.close()
+end
+
+local function handle_station_add()
+    rednet.host("add station", hostname)
+
+    while true do
+        local station_data
+        repeat
+            local id, message = rednet.receive("add station")
+            if stations[id] == nil then
+                station_data = message --[[@as table]]
+            end
+        until stations[id] == nil
+
+        station_data["id"] = #stations
+        table.insert(stations, station_data)
+
+        rednet.broadcast(station_data, "stations")
+    end
+end
+
+local function handle_station_requests()
+    repeat
+        sleep(0.5)
+    until false
+end
+
+read_station_file()
+
+parallel.waitForAny(handle_station_add, handle_station_requests)
