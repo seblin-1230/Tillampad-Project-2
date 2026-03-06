6dbd90a824fc60ec8fae5ce726989dd5c33af898
seblin-1230 <sebastian.lindqvist@elev.ga.ntig.se>
2026-03-06 11:48:54 +0100

    More testing and playing with string.pack

--- program/sha256.lua
+++ program/sha256.lua
@@ -1,16 +1,28 @@
 local utils = require("utils")
 
+local function pad(message, pad)
+    for i = 1, pad do
+        table.insert(message, 0)
+    end
+end
+
 local function generate_message_block(message)
     local message_block = {}
+    local message_length = 0
     for i = 1, #message do
         table.insert(message_block, string.byte(string.sub(message, i)))
+        message_length = message_length + 8
     end
+    table.insert(message_block, 1)
 
+    local pad_by = (64 - #message_block % 64) - 8
+    pad(message_block, pad_by)
+
+    utils.print_table_as_hex(message_block, 2)
 end
 
 local function hash(message)
     local message_block = generate_message_block(message)
 end
-
 
 return {hash = hash}
--- program/testing.lua
+++ program/testing.lua
@@ -1,5 +1,6 @@
 local chacha20 = require("chacha20")
 local sha256 = require("sha256")
+local utils = require("utils")
 
 -- local key = string.char(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f)
 -- local plaintext = [[
@@ -15,3 +16,8 @@ local sha256 = require("sha256")
 local message = "The quick brown fox jumps over the lazy dog and then decides to jump over the lazy dog again just for the fun."
 
 print(sha256.hash(message))
+utils.print_table_as_hex({string.byte("Hello", 1, 5)}, 2)
+
+---@diagnostic disable-next-line: deprecated
+local packed = string.pack("<I8", 1)
+utils.print_table_as_hex({string.byte(packed, 1, #packed)}, 2)
