bef2fa4550b43bd10d35aa3073802b1c4f0a73a9
seblin-1230 <sebastian.lindqvist@elev.ga.ntig.se>
2026-03-09 10:54:02 +0100

    Add function to create message schedule

--- program/sha256.lua
+++ program/sha256.lua
@@ -1,5 +1,9 @@
 local utils = require("utils")
 
+local K = {
+
+}
+
 local function pad(message, pad)
     for i = 1, pad do
         table.insert(message, 0)
@@ -27,13 +31,45 @@ local function generate_message_block(message)
     return message_block
 end
 
+local function message_schedule(chunk)
+    local schedule = {}
+    for i = 1, #chunk, 4 do
+        local word = bit32.bor(bit32.lshift(chunk[i], 24), bit32.lshift(chunk[i+1], 16), bit32.lshift(chunk[i+2], 6), chunk[i+3])
+        table.insert(schedule, word)
+    end 
+
+    for i = 1, 48 do
+        schedule[i+16] = 0
+    end
+
+    return schedule
+end
+
+local function chunk_loop(chunk, hash_values)
+    local message_schedule = message_schedule(chunk)
+
+    return 
+end
+
 local function hash(message)
     local message_block = generate_message_block(message)
     
+    local hash_values = {
+        0x6a09e667,
+        0xbb67ae85,
+        0x3c6ef372,
+        0xa54ff53a,
+        0x510e527f,
+        0x9b05688c,
+        0x1f83d9ab,
+        0x5be0cd19,
+    }
+
     for i = 1, #message_block, 64 do
         local chunk = {table.unpack(message_block, i, i+63)}
 
-        
+        hash_values = chunk_loop(chunk, hash_values)
+        if i == 1 then utils.print_table_as_hex(hash_values, 4) end
     end
 
 end
